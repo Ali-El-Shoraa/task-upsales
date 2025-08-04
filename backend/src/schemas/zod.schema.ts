2494187ac55b7071befec0af4d936b9af06c256b
Ali-El-Shoraa <ali.m.elshoraa@gmail.com>
2025-08-04 20:25:19 +0300

    fix zod schema in back end

--- backend/src/schemas/zod.schema.ts
+++ backend/src/schemas/zod.schema.ts
@@ -1,15 +1,13 @@
 import { z } from "zod";
 
 export const registerSchema = z.object({
-  fallName: z.string().min(3, "اسم المستخدم يجب أن يحتوي على 3 أحرف على الأقل"),
-  email: z.string().email("صيغة البريد الإلكتروني غير صحيحة"),
-  password: z.string().min(6, "كلمة السر يجب أن تكون 6 أحرف أو أكثر"),
+  fallName: z.string().min(3, "Username must contain at least 3 characters"),
+  email: z.string().email("Invalid email format"),
+  password: z.string().min(6, "Password must be 6 characters or more"),
   age: z.number().int().positive().optional(),
 });
 
 export const loginSchema = z.object({
-  email: z.string().email("صيغة البريد الإلكتروني غير صحيحة"),
-  password: z.string().min(6, "كلمة السر يجب أن تكون 6 أحرف أو أكثر"),
+  email: z.string().email("Invalid email format"),
+  password: z.string().min(6, "Password must be 6 characters or more"),
 });
-
-// يمكنك إضافة المزيد من المخططات حسب الحاجة
